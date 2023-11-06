f1cc473396e396d06f72f28e4d002d0165f6be27
danibarranqueroo <danielbo2001@gmail.com>
2023-11-06 18:49:53 +0100

    Estructura de la clase usuario y sus propiedades, closes #8

---
+++ src/Usuario.ts
@@ -0,0 +1,15 @@
+// Clase Usuario
+
+export class Usuario {
+    public rol: ;
+    public actitud: ;
+    public ratio: ;
+    public rango: ;
+  
+    constructor(rol: , actitud: , ratio: , rango: ) {
+      this.rol = rol;
+      this.actitud = actitud;
+      this.ratio = ratio;
+      this.rango = rango;
+    }
+  }
