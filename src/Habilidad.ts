080c3ec01e16c0ee16356e6ef0e3762268ad942e
danibarranqueroo <danielbo2001@gmail.com>
2023-11-06 18:51:57 +0100

    Estructura de la clase habilidad y sus propiedades, closes #9

---
+++ src/Habilidad.ts
@@ -0,0 +1,11 @@
+// Clase Habilidad
+
+export class Habilidad {
+    public tipo: ;
+    public nivel: ;
+  
+    constructor(tipo: , nivel: ) {
+      this.tipo = tipo;
+      this.nivel = nivel;
+    }
+  }
