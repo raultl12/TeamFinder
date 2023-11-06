358f6e96c4a93b40475ad4d98667e26003a931d0
danibarranqueroo <danielbo2001@gmail.com>
2023-11-06 18:54:01 +0100

    Guardamos los tipos de rol como un enum, closes #10

--- src/Usuario.ts
+++ src/Usuario.ts
@@ -1,12 +1,19 @@
 // Clase Usuario
 
+export enum Rol {
+    Fragger = "Fragger",
+    IGL = "IGL",
+    Support = "Support",
+    Lurker = "Lurker",
+    Awper = "Awper",
+  }
 export class Usuario {
-    public rol: ;
+    public rol: Rol;
     public actitud: ;
     public ratio: ;
     public rango: ;
   
-    constructor(rol: , actitud: , ratio: , rango: ) {
+    constructor(rol: Rol, actitud: , ratio: , rango: ) {
       this.rol = rol;
       this.actitud = actitud;
       this.ratio = ratio;
