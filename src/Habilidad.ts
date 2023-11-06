dd31511fed56684b3e54c6fa2f0dec76092d5883
danibarranqueroo <danielbo2001@gmail.com>
2023-11-06 19:34:49 +0100

    Guardamos los tipos de nivel como un enum, closes #15

--- src/Habilidad.ts
+++ src/Habilidad.ts
@@ -5,11 +5,20 @@ export enum TipoHabilidad {
     Rotaciones = "Rotaciones",
     Apoyo = "Apoyo",
   }
+
+export enum Nivel {
+    Muy_malo = "Muy malo",
+    Malo = "Malo",
+    Medio = "Medio",
+    Bueno = "Bueno",
+    Muy_bueno = "Muy bueno",
+    Excelente = "Excelente",
+}
 export class Habilidad {
     public tipo: TipoHabilidad;
-    public nivel: ;
+    public nivel: Nivel;
   
-    constructor(tipo: TipoHabilidad, nivel: ) {
+    constructor(tipo: TipoHabilidad, nivel: Nivel) {
       this.tipo = tipo;
       this.nivel = nivel;
     }
