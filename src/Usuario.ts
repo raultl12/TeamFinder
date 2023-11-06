8945eb08b8eba3dc346d24be8c32b9ad64360bf2
danibarranqueroo <danielbo2001@gmail.com>
2023-11-06 19:00:57 +0100

    Guardamos los tipos de rango como un enum y finalizamos clase usuario, closes #13

--- src/Usuario.ts
+++ src/Usuario.ts
@@ -12,13 +12,22 @@ export enum Actitud {
     Agresivo = "Agresivo",
     Controlador = "Controlador",
   }
+
+export enum Rango {
+    Plata = "Plata",
+    Oro = "Oro",
+    Ak = "Ak",
+    Maestro = "Maestro",
+    EliteGlobal = "Elite Global",
+  }
+  
 export class Usuario {
     public rol: Rol;
     public actitud: Actitud;
     public ratio: number;
-    public rango: ;
+    public rango: Rango;
   
-    constructor(rol: Rol, actitud: Actitud, ratio: , rango: ) {
+    constructor(rol: Rol, actitud: Actitud, ratio: number, rango: Rango) {
       this.rol = rol;
       this.actitud = actitud;
       this.ratio = ratio;
