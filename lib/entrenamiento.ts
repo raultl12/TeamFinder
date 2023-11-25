5468c2190180a73e4bc1436a62ae0d2ae10c68e4
danibarranqueroo <danielbo2001@gmail.com>
2023-11-25 16:21:20 +0100

    Creación de entidad y actualizao iv.yaml
    Es necesario una entidad que gestione Usuario y Habilidad así que creo Entrenamiento para ello

---
+++ lib/entrenamiento.ts
@@ -0,0 +1,10 @@
+// Clase Entrenamiento
+
+import { Usuario } from './usuario'; // Importar la clase Usuario
+import { Habilidad } from './habilidad'; // Importar la clase Habilidad
+
+class Entrenamiento {
+  private usuario: Usuario;
+  private habilidad: Habilidad;
+
+  }
