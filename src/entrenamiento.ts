389b68279cf50ddd093aaa6032301874bdced379
raultl12 <raultl6212@gmail.com>
2023-12-01 12:58:04 +0100

    feat: añadidos getters a usuario y entrenamiento closes #46
    Se han añadido getters a las clases usuario y entrenamiento para que la lógica de negocio pueda leer estos atributos

--- src/entrenamiento.ts
+++ src/entrenamiento.ts
@@ -6,4 +6,25 @@ export class Entrenamiento{
     private rango: Rango;
     private tipoHabilidad: TipoHabilidad;
     private nivel: Nivel;
+
+    //Getters de los campos
+    public getRol(): Rol{
+        return this.rol;
+    }
+    
+    public getActitud(): Actitud{
+        return this.actitud;
+    }
+
+    public getRango(): Rango{
+        return this.rango;
+    }
+
+    public getTipoHabilidad(): TipoHabilidad{
+        return this.tipoHabilidad;
+    }
+
+    public getNivel(): Nivel{
+        return this.nivel;
+    }
 }
--- src/usuario.ts
+++ src/usuario.ts
@@ -5,4 +5,17 @@ export class Usuario {
     private actitud: Actitud;
     private ratio: number;
     private rango: Rango;
+
+    public getRol(): Rol{
+        return this.rol;
+    }
+    public getActitud(): Actitud{
+        return this.actitud;
+    }
+    public getRatio(): number{
+        return this.ratio;
+    }
+    public getRango(): Rango{
+        return this.rango;
+    }
 }
