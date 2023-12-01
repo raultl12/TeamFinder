fbfd2658623ce87b9addac8751f8e8d4abbc2e69
danibarranqueroo <danielbo2001@gmail.com>
2023-12-01 16:27:50 +0100

    Creo fichero para los enums, closes #48
    Los enums que estaban en usuario y habilidad se pasan a un fichero aparte

--- lib/habilidad.ts
+++ lib/habilidad.ts
@@ -1,17 +1,4 @@
-export enum TipoHabilidad {
-    Posicionamiento,
-    Rotaciones,
-    Apoyo,
-  }
-
-export enum Nivel {
-    Muy_malo,
-    Malo,
-    Medio,
-    Bueno,
-    Muy_bueno,
-    Excelente,
-}
+import { TipoHabilidad, Nivel } from "./types";
 export class Habilidad {
     public tipo: TipoHabilidad;
     public nivel: Nivel;
---
+++ lib/types.ts
@@ -0,0 +1,35 @@
+export enum Rol {
+    Fragger,
+    IGL,
+    Support,
+    Lurker,
+    Awper,
+  }
+
+export enum Actitud {
+    Agresivo,
+    Controlador,
+  }
+
+export enum Rango {
+    Plata,
+    Oro,
+    Ak,
+    Maestro,
+    EliteGlobal,
+  }
+
+export enum TipoHabilidad {
+    Posicionamiento,
+    Rotaciones,
+    Apoyo,
+  }
+
+export enum Nivel {
+    Muy_malo,
+    Malo,
+    Medio,
+    Bueno,
+    Muy_bueno,
+    Excelente,
+}
