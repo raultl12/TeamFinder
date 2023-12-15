fb024ba3b5b09db580d879adcead2d70a81f6844
raultl12 <raultl6212@gmail.com>
2023-12-15 13:24:29 +0100

    refactor: Corregidos test closes #65
    Se han corregido los test tras el cambio del constructor del asignador

--- test/test_asignador.ts
+++ test/test_asignador.ts
@@ -6,94 +6,42 @@ import { Entrenamiento } from "../src/entrenamiento.ts";
 import { Usuario } from "../src/usuario.ts";
 import { Rol, Actitud, Rango, TipoHabilidad, Nivel } from "../src/types.ts";
 
+let entrenamiento1 = new Entrenamiento(Rol.Awper, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Posicionamiento, Nivel.Medio);
+let entrenamiento2 = new Entrenamiento(Rol.Awper, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Apoyo, Nivel.Muy_bueno);
+let entrenamiento3 = new Entrenamiento(Rol.Fragger, Actitud.Controlador, Rango.Maestro, TipoHabilidad.Rotaciones, Nivel.Excelente);
+let entrenamiento4 = new Entrenamiento(Rol.IGL, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Posicionamiento, Nivel.Muy_bueno);
+let entrenamiento5 = new Entrenamiento(Rol.Lurker, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Apoyo, Nivel.Muy_bueno);
+let entrenamiento6 = new Entrenamiento(Rol.Awper, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Posicionamiento, Nivel.Muy_bueno);
+let entrenamiento7 = new Entrenamiento(Rol.Support, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Rotaciones, Nivel.Muy_bueno);
+
+let entrenamientos = [entrenamiento1, entrenamiento2, entrenamiento3,
+  entrenamiento4, entrenamiento5, entrenamiento6, entrenamiento7];
+
 // @ts-ignore
 Deno.test("Test de asignación 1", () => {
 
-  let asignador = new Asignador();
-
-  asignador.setFiltroHabilidad(TipoHabilidad.Posicionamiento);
 
   let usuario = new Usuario(Rol.Awper, Actitud.Agresivo, 1.3, Rango.Maestro);
 
-  let entrenamiento1 = new Entrenamiento(Rol.Awper, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Posicionamiento, Nivel.Medio);
-  let entrenamiento2 = new Entrenamiento(Rol.Awper, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Apoyo, Nivel.Muy_bueno);
-  let entrenamiento3 = new Entrenamiento(Rol.Fragger, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Apoyo, Nivel.Muy_bueno);
-  let entrenamiento4 = new Entrenamiento(Rol.IGL, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Posicionamiento, Nivel.Muy_bueno);
-  let entrenamiento5 = new Entrenamiento(Rol.Lurker, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Apoyo, Nivel.Muy_bueno);
-  let entrenamiento6 = new Entrenamiento(Rol.Awper, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Posicionamiento, Nivel.Muy_bueno);
-  let entrenamiento7 = new Entrenamiento(Rol.Support, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Rotaciones, Nivel.Muy_bueno);
-
-  asignador.setUsuario(usuario);
-
-  asignador.aniadirEntrenamiento(entrenamiento1);
-  asignador.aniadirEntrenamiento(entrenamiento2);
-  asignador.aniadirEntrenamiento(entrenamiento3);
-  asignador.aniadirEntrenamiento(entrenamiento4);
-  asignador.aniadirEntrenamiento(entrenamiento5);
-  asignador.aniadirEntrenamiento(entrenamiento6);
-  asignador.aniadirEntrenamiento(entrenamiento7);
-
+  let asignador = new Asignador(usuario, entrenamientos, TipoHabilidad.Posicionamiento);
+  
   assertEquals(asignador.asignarEntrenamiento(), entrenamiento1);
 });
 
 // @ts-ignore
 Deno.test("Test de asignación 2", () => {
-
-  let asignador = new Asignador();
-
-  asignador.setFiltroHabilidad(TipoHabilidad.Rotaciones);
-
   let usuario = new Usuario(Rol.Fragger, Actitud.Controlador, 3, Rango.Maestro);
 
-  let entrenamiento1 = new Entrenamiento(Rol.Awper, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Posicionamiento, Nivel.Medio);
-  let entrenamiento2 = new Entrenamiento(Rol.Awper, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Apoyo, Nivel.Muy_bueno);
-  let entrenamiento3 = new Entrenamiento(Rol.Fragger, Actitud.Controlador, Rango.Maestro, TipoHabilidad.Rotaciones, Nivel.Excelente);
-  let entrenamiento4 = new Entrenamiento(Rol.IGL, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Posicionamiento, Nivel.Muy_bueno);
-  let entrenamiento5 = new Entrenamiento(Rol.Lurker, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Apoyo, Nivel.Muy_bueno);
-  let entrenamiento6 = new Entrenamiento(Rol.Awper, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Posicionamiento, Nivel.Muy_bueno);
-  let entrenamiento7 = new Entrenamiento(Rol.Support, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Rotaciones, Nivel.Muy_bueno);
-
-  asignador.setUsuario(usuario);
-
-  asignador.aniadirEntrenamiento(entrenamiento1);
-  asignador.aniadirEntrenamiento(entrenamiento2);
-  asignador.aniadirEntrenamiento(entrenamiento3);
-  asignador.aniadirEntrenamiento(entrenamiento4);
-  asignador.aniadirEntrenamiento(entrenamiento5);
-  asignador.aniadirEntrenamiento(entrenamiento6);
-  asignador.aniadirEntrenamiento(entrenamiento7);
+  let asignador = new Asignador(usuario, entrenamientos, TipoHabilidad.Rotaciones);
 
   assertEquals(asignador.asignarEntrenamiento(), entrenamiento3);
 });
 
 // @ts-ignore
 Deno.test("Comparacion: Lógica vs Entrenamiento aleatorio", () => {
-
-  let asignador = new Asignador();
-
-  asignador.setFiltroHabilidad(TipoHabilidad.Apoyo);
-
   let usuario = new Usuario(Rol.Support, Actitud.Controlador, 1.3, Rango.Maestro);
 
-  let entrenamiento1 = new Entrenamiento(Rol.Awper, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Posicionamiento, Nivel.Medio);
-  let entrenamiento2 = new Entrenamiento(Rol.Awper, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Apoyo, Nivel.Muy_bueno);
-  let entrenamiento3 = new Entrenamiento(Rol.Fragger, Actitud.Controlador, Rango.Maestro, TipoHabilidad.Rotaciones, Nivel.Excelente);
-  let entrenamiento4 = new Entrenamiento(Rol.IGL, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Posicionamiento, Nivel.Muy_bueno);
-  let entrenamiento5 = new Entrenamiento(Rol.Lurker, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Apoyo, Nivel.Muy_bueno);
-  let entrenamiento6 = new Entrenamiento(Rol.Awper, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Posicionamiento, Nivel.Muy_bueno);
-  let entrenamiento7 = new Entrenamiento(Rol.Support, Actitud.Agresivo, Rango.Maestro, TipoHabilidad.Rotaciones, Nivel.Muy_bueno);
-
-  asignador.setUsuario(usuario);
-
-  asignador.aniadirEntrenamiento(entrenamiento1);
-  asignador.aniadirEntrenamiento(entrenamiento2);
-  asignador.aniadirEntrenamiento(entrenamiento3);
-  asignador.aniadirEntrenamiento(entrenamiento4);
-  asignador.aniadirEntrenamiento(entrenamiento5);
-  asignador.aniadirEntrenamiento(entrenamiento6);
-  asignador.aniadirEntrenamiento(entrenamiento7);
-
-  
+  let asignador = new Asignador(usuario, entrenamientos, TipoHabilidad.Apoyo); 
   // Obtener un entrenamiento aleatorio
   const numeroAleatorio = Math.floor(Math.random() * 7);
   let entrenamientoAleatorio = asignador.getEntrenamientos()[numeroAleatorio];
